42383f688ab99b352a61d46c57ad82fb3b8d539f
Julia Himmel <himmel.julia@gmail.com>
2014-06-19 22:34:50 -0400

    moves list items from one column to other, but needs refactoring

--- js/application.js
+++ js/application.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
 	$(".col_1 ul li").on("click", function(event){
 		event.preventDefault();
 		var listItem = this;
+    $('.col_1 ul li').parentsUntil('body', '.col_1').siblings().children('ul').append(listItem);
 	})
 });
+
+
+
